1621f59ba00635fb33d1e535d830ddf368fb2db4
And3 <arbadev@gmail.com>
2018-01-22 22:23:55 -0400

    added redux dev tool

--- src/store/StoreConfig.js
+++ src/store/StoreConfig.js
@@ -1,14 +1,19 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { apiMiddleware } from 'redux-api-middleware'
 import promise from 'redux-promise'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 
 import reducers from '../reducers'
 
 export default function configureStore(initialState) {
   const finalCreateStore = compose(applyMiddleware(promise, apiMiddleware))(createStore)
 
-  const store = finalCreateStore(reducers, initialState)
-
+  const store = finalCreateStore(
+    reducers,
+    initialState,
+    composeWithDevTools(),
+  )
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {
